ab0528ef286f28ca953105c0018cd7d3c272fd9a
josMuiruri <josemuiruri51@gmail.com>
2024-07-04 19:35:49 +0300

    documenting the code

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,17 @@
+#!/usr/bin/env python3
+"""
+simple Flask web application that displays a welcome message
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    View function for the root URL("/")
+    Renders the index.html which displays a welcome message
+    """
     return render_template('0-index.html')
 
 if __name__ == '__main__':
