ff704f9deb769b3dc7d967c8d813792ad689d7c7
josMuiruri <josemuiruri51@gmail.com>
2024-08-29 18:04:22 +0300

    trailing whitespace

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 '''pagination helper function'''
 from typing import Tuple
-
-
 ''' a func that takes 2 int args & return a tuple containing 
 a start & an end corresponding to the range of indexes'''
 
