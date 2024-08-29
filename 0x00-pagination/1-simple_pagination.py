b1a4a22d72e29087ea104c518e3a9271147cc03a
josMuiruri <josemuiruri51@gmail.com>
2024-08-29 18:31:51 +0300

    Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+'''Simple pagination '''
+import csv
+import math
+from typing import Tuple, List
+'''a func that retrieves the index range from a given page and page size'''
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    ''' assigning a start index '''
+    start_index = (page - 1) * page_size
+    ''' assigning an end index '''
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''Retrieves a page of data
+        '''
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+        if start_index > len(data):
+            return []
+        return data[start_index:end_index]
