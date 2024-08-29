ec381d96d79bf7825899f97b9a3f4d8e4d9b7ecb
josMuiruri <josemuiruri51@gmail.com>
2024-08-29 17:58:19 +0300

    document the module

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+'''pagination helper function'''
 from typing import Tuple
 ''' a func that takes 2 int args & return a tuple containing 
 a start & an end corresponding to the range of indexes'''
@@ -10,8 +11,3 @@ def index_range(page: int, page_size: int) -> Tuple:
     # assigning an end index
     end_index = start_index + page_size
     return start_index, end_index
-
-
-# res = index_range(1, 7)
-# print(type(res))
-# print(res)
