62831dc6f004424c3401aa07bcf482f198e692b6
josMuiruri <josemuiruri51@gmail.com>
2024-08-29 18:06:30 +0300

    trailing whitespace

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,8 @@
 '''pagination helper function'''
 from typing import Tuple
 ''' a func that takes 2 int args & return a tuple containing 
-a start & an end corresponding to the range of indexes'''
+a start & an end corresponding to the range of indexes
+'''
 
 
 def index_range(page: int, page_size: int) -> Tuple:
